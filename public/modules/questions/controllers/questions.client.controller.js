4b347f30547ca0e1a85b7a6c70af5e563df68215
Brett Koenig <koenig.337@osu.edu>
2014-11-03 21:31:16 -0500

    furthest, controllers broken apart and questions not saving

--- public/modules/questions/controllers/questions.client.controller.js
+++ public/modules/questions/controllers/questions.client.controller.js
@@ -1,81 +1,65 @@
 'use strict';
 
-var questionsApp = angular.module('questions');
 // Questions controller
-questionsApp.controller('QuestionsController', ['$scope', '$stateParams', 'Authentication', 'Questions',
-	function($scope, $stateParams, Authentication, Questions ) {
+angular.module('questions').controller('QuestionsController', ['$scope', '$stateParams', '$location', 'Authentication', 'Questions',
+	function($scope, $stateParams, $location, Authentication, Questions ) {
+		$scope.authentication = Authentication;
 
-this.authentication = Authentication;
-// Find a list of Questions
+		// Create new Question
+		$scope.create = function() {
+			// Create new Question object
+			var question = new Questions ({
+				question: this.question
+			});
 
-this.questions = Questions.query();
+			// Redirect after save
+			question.$save(function(response) {
+				$location.path('questions/' + response._id);
 
-		}
+				// Clear form fields
+				$scope.question = '';
+			}, function(errorResponse) {
+				$scope.error = errorResponse.data.message;
+			});
+		};
+
+		// Remove existing Question
+		$scope.remove = function( question ) {
+			if ( question ) { question.$remove();
+
+				for (var i in $scope.questions ) {
+					if ($scope.questions [i] === question ) {
+						$scope.questions.splice(i, 1);
+					}
+				}
+			} else {
+				$scope.question.$remove(function() {
+					$location.path('questions');
+				});
+			}
+		};
+
+		// Update existing Question
+		$scope.update = function() {
+			var question = $scope.question ;
+
+			question.$update(function() {
+				$location.path('questions/' + question._id);
+			}, function(errorResponse) {
+				$scope.error = errorResponse.data.message;
+			});
+		};
+
+		// Find a list of Questions
+		$scope.find = function() {
+			$scope.questions = Questions.query();
+		};
+
+		// Find existing Question
+		$scope.findOne = function() {
+			$scope.question = Questions.get({ 
+				questionId: $stateParams.questionId
+			});
+		};
+	}
 ]);
-
-questionsApp.controller('QuestionsCreateController', ['$scope','Questions',
-	function($scope,  Questions ) {
-
-		}
-]);
-
-questionsApp.controller('QuestionsEditController', ['$scope', 'Questions',
-	function($scope,  Questions ) {
-
-		}
-]);
-
-		// // Create new Question
-		// $scope.create = function() {
-		// 	// Create new Question object
-		// 	var question = new Questions ({
-		// 		question: this.question
-		// 	});
-
-		// 	// Redirect after save
-		// 	question.$save(function(response) {
-		// 		$location.path('questions/' + response._id);
-
-		// 		// Clear form fields
-		// 		$scope.question = '';
-		// 	}, function(errorResponse) {
-		// 		$scope.error = errorResponse.data.message;
-		// 	});
-		// };
-
-		// // Remove existing Question
-		// $scope.remove = function( question ) {
-		// 	if ( question ) { question.$remove();
-
-		// 		for (var i in $scope.questions ) {
-		// 			if ($scope.questions [i] === question ) {
-		// 				$scope.questions.splice(i, 1);
-		// 			}
-		// 		}
-		// 	} else {
-		// 		$scope.question.$remove(function() {
-		// 			$location.path('questions');
-		// 		});
-		// 	}
-		// };
-
-		// // Update existing Question
-		// $scope.update = function() {
-		// 	var question = $scope.question ;
-
-		// 	question.$update(function() {
-		// 		$location.path('questions/' + question._id);
-		// 	}, function(errorResponse) {
-		// 		$scope.error = errorResponse.data.message;
-		// 	});
-		// };
-
-		
-
-		// // Find existing Question
-		// $scope.findOne = function() {
-		// 	$scope.question = Questions.get({ 
-		// 		questionId: $stateParams.questionId
-		// 	});
-		// };
-	
